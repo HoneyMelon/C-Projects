b215f2cda39e1432e90acdbb5e58b237ee6cd0c1
Valentina Vef <lauvef@students.uni-mainz.de>
2020-04-28 12:58:54 +0200

    Stuff

--- gcd.cpp
+++ gcd.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Lili on 4/26/2020.
-//
+#include <iostream>
 
+int gcd(int a, int b) {
+    while(a != b) {
+        if (a > b) {
+            a -= b;
+        }
+        else {
+            b -= a;
+        }
+    }
+    return a;
+}
+
+int main(){
+    int result = gcd(30, 6);
+    std::cout << result <<std::endl;
+    return 0;
+}
--- pi.cpp
+++ pi.cpp
@@ -1,4 +1,29 @@
-//
-// Created by Lili on 4/27/2020.
-//
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+
+int main() {
+    uint64_t draws = 100000000;
+    uint64_t in_circle = 0;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0, 1.0);
+
+    for (uint64_t i=0; i < draws; ++i) {
+        double x = dis(gen);
+        double y = dis(gen);
+
+        if (x * x + y * y < 1) {
+            ++in_circle;
+        }
+
+    }
+    double pi = static_cast<double>(in_circle) / draws * 4;
+    // double pi = 1.0 in_circle / draws * 4;       works too
+
+    std::cout << std::setprecision(13) << pi << std::endl;
+    return 0;
+}
 
